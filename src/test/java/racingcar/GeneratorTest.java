2ab9c7ec8ee43e9ae8f1cd3e8241380c7a226f45
jun <ggprgrkjh@naver.com>
2023-11-24 02:17:12 +0900

    feat(generator): 랜덤 번호를 생성한다.

---
+++ src/test/java/racingcar/GeneratorTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Generator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GeneratorTest {
+
+    @DisplayName("랜덤 번호 생성 정상 작동 테스트")
+    @Test
+    void generateRandomNumber_EqualResult_Success() {
+        //given
+        int randomNumber;
+        //when
+        randomNumber = Generator.generateRandomNumber();
+        //then
+        assertThat(randomNumber).isNotEqualTo(0);
+        assertThat(randomNumber).isIn(1, 9);
+    }
+}
