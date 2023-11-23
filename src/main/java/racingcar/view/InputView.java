e395b30c79085a2ce25ab716eb2ea8a7f50e75ff
jun <ggprgrkjh@naver.com>
2023-11-24 01:53:42 +0900

    feat(inputview): 시도할 회수를 입력받는다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,5 +9,10 @@ public class InputView {
         return Console.readLine();
     }
 
+    public static String readTryNumber() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Console.readLine();
+    }
+
 
 }
