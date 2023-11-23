7bb837342c8c9dced98985afbe3b6b6738e4cb56
jun <ggprgrkjh@naver.com>
2023-11-24 03:27:11 +0900

    feat(inputview): 메세지 상수 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,17 +8,20 @@ public class InputView {
 
     private static final String NAME_PATTERN = "^(?:[a-zA-Z가-힣]+,)*[a-zA-Z가-힣]+$";
     private static final String NUMBER_PATTERN = "^[0-9]+$";
+    private static final String PARTICIPANTS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRYNUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String INVALID_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값이 입력되었습니다.";
 
 
     public static String readParticipants() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(PARTICIPANTS_MESSAGE);
         String input = Console.readLine();
         validateNameFormat(input);
         return input;
     }
 
     public static String readTryNumber() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(TRYNUMBER_MESSAGE);
         String input = Console.readLine();
         validateIsInteger(input);
         return input;
@@ -27,13 +30,13 @@ public class InputView {
 
     public static void validateNameFormat(String input) {
         if (!Pattern.matches(NAME_PATTERN, input)) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다.");
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
         }
     }
 
     public static void validateIsInteger(String input) {
         if (!Pattern.matches(NUMBER_PATTERN, input)) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다.");
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
         }
     }
 
