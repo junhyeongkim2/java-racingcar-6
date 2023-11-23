b940125b766e35b072e740d3edeec7640b4d94c2
jun <ggprgrkjh@naver.com>
2023-11-24 02:33:46 +0900

    feat(inputview): 이름이 올바르지 않은 형식으로 입력될 경우 에러 발생

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,17 +1,29 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class InputView {
 
+    private static final String NAME_PATTERN = "^(?:[a-zA-Z가-힣]+,)*[a-zA-Z가-힣]+$";
+
     public static String readParticipants() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        String input = Console.readLine();
+        validateNameFormat(input);
+        return input;
     }
 
     public static String readTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Console.readLine();
+    }
+
+
+    public static void validateNameFormat(String input) {
+        if (!Pattern.matches(NAME_PATTERN, input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다.");
+        }
     }
 
 
