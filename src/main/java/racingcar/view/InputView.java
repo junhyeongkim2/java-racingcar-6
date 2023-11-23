52f4263007437710f18023b826890d55c2f7ad44
jun <ggprgrkjh@naver.com>
2023-11-24 02:45:08 +0900

    feat(inputview): 시도할 회수가 숫자가 아니라면 에러가 발생한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,10 +2,13 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.regex.Pattern;
+import net.bytebuddy.pool.TypePool.Resolution.Illegal;
 
 public class InputView {
 
     private static final String NAME_PATTERN = "^(?:[a-zA-Z가-힣]+,)*[a-zA-Z가-힣]+$";
+    private static final String NUMBER_PATTERN = "^[0-9]+$";
+
 
     public static String readParticipants() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -16,12 +19,20 @@ public class InputView {
 
     public static String readTryNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return Console.readLine();
+        String input = Console.readLine();
+        validateIsInteger(input);
+        return input;
     }
 
 
     public static void validateNameFormat(String input) {
         if (!Pattern.matches(NAME_PATTERN, input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다.");
+        }
+    }
+
+    public static void validateIsInteger(String input) {
+        if (!Pattern.matches(NUMBER_PATTERN, input)) {
             throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다.");
         }
     }
